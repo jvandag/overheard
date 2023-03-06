6498fc568e79bdd2f6552a64a33b5c5f83931ed3
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2023-03-05 21:59:39 -0800

    feat: track script coverage

--- scripts/sqlite.sh
+++ scripts/sqlite.sh
@@ -1,44 +1,55 @@
 #!/usr/bin/env bash
 
-FILEPATH="$1"
-POLL_SECONDS="30"
+export OVERHEARD_SQLITE_FILEPATH="$1" \
+       OVERHEARD_SQLITE_POLL_SECONDS="30"
 
-insert() {
+overheard_sqlite_insert() {
   local time="$(date +"%d/%m/%Y-%T")"
   case "$1" in
     "online:" )
       echo "$time online $2 $3"
-      sqlite3 "$FILEPATH" \
+      sqlite3 "$OVERHEARD_SQLITE_FILEPATH" \
         "INSERT INTO online (count, date) VALUES ('$2', '$time');"
       ;;
     "scroll:" )
       echo "$time scroll $2 $3"
-      sqlite3 "$FILEPATH" \
+      sqlite3 "$OVERHEARD_SQLITE_FILEPATH" \
         "INSERT INTO alerts (name, phase, date) VALUES ('$2', '$3', '$time');"
     ;;
   esac
 }
 
-listen() {
-  node dist/index.js -t "${POLL_SECONDS}s" | xargs -I {} bash -c 'insert {}'
+overheard_sqlite_boot() {
+  local time="$(date +"%d/%m/%Y-%T")"
+  echo "$1 $time"
+  sqlite3 "$OVERHEARD_SQLITE_FILEPATH" "INSERT INTO boot (type, date) VALUES ('$1', '$time');"
 }
 
-export -f insert
-export FILEPATH
+overheard_sqlite_listen() {
+  node dist/index.js -t "${OVERHEARD_SQLITE_POLL_SECONDS}s" \
+    | xargs -I {} bash -c 'overheard_sqlite_insert {}'
+}
+
+export -f overheard_sqlite_insert
 
 # Main
-[[ "${FILEPATH##*.}" != "sqlite" ]] \
-  && echo "Error: path \"$FILEPATH\" is invalid!" >&2 \
+[[ "${OVERHEARD_SQLITE_FILEPATH##*.}" != "sqlite" ]] \
+  && echo "Error: path \"$OVERHEARD_SQLITE_FILEPATH\" is invalid!" >&2 \
   && exit 1
 
 # Create tables
-sqlite3 "$FILEPATH" <<EOF
+sqlite3 "$OVERHEARD_SQLITE_FILEPATH" <<EOF
+CREATE TABLE IF NOT EXISTS boot (id INTEGER PRIMARY KEY, type TEXT, date TEXT);
 CREATE TABLE IF NOT EXISTS alerts (id INTEGER PRIMARY KEY, name TEXT, phase TEXT, date TEXT);
 CREATE TABLE IF NOT EXISTS online (id INTEGER PRIMARY KEY, count INTEGER, date TEXT);
 EOF
 
 # Start listener
-until listen; do
-  echo "Scraper crashed with code $? restarting..." >&2 \
-    && sleep "$POLL_SECONDS"
+trap "overheard_sqlite_boot 'exit'" EXIT \
+  && overheard_sqlite_boot 'start'
+
+until overheard_sqlite_listen; do
+  echo "Scraper crashed with code: \"$?\" restarting..." >&2 \
+    && sleep "$OVERHEARD_SQLITE_POLL_SECONDS" \
+    && overheard_sqlite_boot 'start'
 done
