926c40c8ac03489f6cceae572c883edb58b4034c
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2023-03-05 21:40:10 -0800

    feat: use text dates

--- scripts/sqlite.sh
+++ scripts/sqlite.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 FILEPATH="$1"
+POLL_SECONDS="30"
 
 insert() {
   local time="$(date +"%d/%m/%Y-%T")"
-  local date="$(date +%s)"
   case "$1" in
     "online:" )
       echo "$time online $2 $3"
       sqlite3 "$FILEPATH" \
-        "INSERT INTO online (count, date) VALUES ('$2', '$date');"
+        "INSERT INTO online (count, date) VALUES ('$2', '$time');"
       ;;
     "scroll:" )
       echo "$time scroll $2 $3"
       sqlite3 "$FILEPATH" \
-        "INSERT INTO alerts (name, phase, date) VALUES ('$2', '$3', '$date');"
+        "INSERT INTO alerts (name, phase, date) VALUES ('$2', '$3', '$time');"
     ;;
   esac
 }
 
 listen() {
-  node dist/index.js -t 10s | xargs -I {} bash -c 'insert {}'
+  node dist/index.js -t "${POLL_SECONDS}s" | xargs -I {} bash -c 'insert {}'
 }
 
 export -f insert
@@ -33,12 +33,12 @@ export FILEPATH
 
 # Create tables
 sqlite3 "$FILEPATH" <<EOF
-CREATE TABLE IF NOT EXISTS alerts (id INTEGER PRIMARY KEY, name TEXT, phase TEXT, date INTEGER);
-CREATE TABLE IF NOT EXISTS online (id INTEGER PRIMARY KEY, count INTEGER, date INTEGER);
+CREATE TABLE IF NOT EXISTS alerts (id INTEGER PRIMARY KEY, name TEXT, phase TEXT, date TEXT);
+CREATE TABLE IF NOT EXISTS online (id INTEGER PRIMARY KEY, count INTEGER, date TEXT);
 EOF
 
 # Start listener
 until listen; do
   echo "Scraper crashed with code $? restarting..." >&2 \
-    && sleep 10
+    && sleep "$POLL_SECONDS"
 done
