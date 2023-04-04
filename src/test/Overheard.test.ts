fb9a56be0ae71575aae34a245fdadb80dd7463dc
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2023-04-03 23:25:14 -0700

    chore: add template function

--- src/test/Overheard.test.ts
+++ src/test/Overheard.test.ts
@@ -2,18 +2,25 @@
 
 import { Overheard } from '../Overheard'
 
-const OVERHEARD_NOTHING =
-  '<html><head><title>Overheard</title></head><body bgcolor="#f5f3e4"><div align="center"><h1>Overheard in Tavelor\'s Tavern</h1></div><div align="left">There are 70 champions adventuring across the realms today, more or less.<br><br>The moon is a waning gibbous.<br><br>There are no reports of glowing or dark scrolls.<br><br></body></html>'
-
-const OVERHEARD_GLOWING =
-  '<html><head><title>Overheard</title></head><body bgcolor="#f5f3e4"><div align="center"><h1>Overheard in Tavelor\'s Tavern</h1></div><div align="left">There are 70 champions adventuring across the realms today, more or less.<br><br>The moon is a waning gibbous.<br><br>Rumor has it that necromancy and conjuration scrolls are glowing.<br><br></body></html>'
+const template = (
+  online = '50',
+  moon = 'new',
+  scrolls = 'There are no reports of glowing or dark scrolls',
+): string =>
+  `<html><head><title>Overheard</title></head><body bgcolor="#f5f3e4"><div align="center"><h1>Overheard in Tavelor's Tavern</h1></div><div align="left">There are ${online} champions adventuring across the realms today, more or less.<br><br>The moon is a ${moon}.<br><br>Rumor has it that ${scrolls}.<br><br></body></html>`
 
 describe('Overheard.', () => {
   it('Should parse properties', () => {
     const overheard = new Overheard({})
-    const res = overheard['parse'](OVERHEARD_GLOWING)
+    const res = overheard['parse'](
+      template(
+        '150',
+        'waning gibbous',
+        'necromancy and conjuration scrolls are glowing',
+      ),
+    )
     expect(res).toStrictEqual({
-      online: 70,
+      online: 150,
       moon: 'waning_gibbous',
       scrolls: [
         { name: 'necromancy', phase: 'glowing' },
@@ -36,7 +43,8 @@ describe('Overheard.', () => {
 
   it('Should emit events', (done) => {
     const overheard = new Overheard({})
-    overheard['fetch'] = async (url) => OVERHEARD_GLOWING
+    overheard['fetch'] = async (url) =>
+      template('50', 'new', 'necromancy and conjuration scrolls are glowing')
     overheard
       .once('scrolls', (scrolls) => {
         expect(scrolls).toStrictEqual([
@@ -64,7 +72,7 @@ describe('Overheard.', () => {
         },
       },
     )
-    overheard['fetch'] = async (url) => OVERHEARD_NOTHING
+    overheard['fetch'] = async (url) => template()
     overheard
       .once('scrolls', (scrolls) => {
         expect(scrolls).toStrictEqual([
